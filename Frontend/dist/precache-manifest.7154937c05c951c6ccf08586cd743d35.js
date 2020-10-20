eaf64709d4afc3e83f1c71981386320af3ef9f3c
X de Dev <pntxall100@gmail.com>
2020-10-20 18:23:43 +0300

    Built test frontend

---
+++ Frontend/dist/precache-manifest.7154937c05c951c6ccf08586cd743d35.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202d502fef274903462e",
+    "url": "/css/app.f1d6a260.css"
+  },
+  {
+    "revision": "7a6776387d69ff7eec6a430072a823d9",
+    "url": "/img/BG.7a677638.png"
+  },
+  {
+    "revision": "284979da4df2a43b9abac4467e3a3849",
+    "url": "/img/Logo.284979da.png"
+  },
+  {
+    "revision": "6292fb499815bfe14e20c5b1812c3a17",
+    "url": "/img/arrow.6292fb49.svg"
+  },
+  {
+    "revision": "a4d258c4787760554dc270a984783cac",
+    "url": "/img/avatar1.a4d258c4.svg"
+  },
+  {
+    "revision": "17a76835c1a94da1de9956d8eae47b15",
+    "url": "/img/pen.17a76835.svg"
+  },
+  {
+    "revision": "a4f57e749de3dc47287eca44a975e2b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "202d502fef274903462e",
+    "url": "/js/app.34fb7834.js"
+  },
+  {
+    "revision": "624ce69a41c08bc75dfd",
+    "url": "/js/chunk-vendors.b55e0152.js"
+  },
+  {
+    "revision": "4b07ff6e03dbfe882300a6d75b89722a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
